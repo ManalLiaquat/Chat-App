5823ed980567a3d1c4d3ada9716167c357dc8be9
ManalLiaquat <m.manalliaquat@gmail.com>
2018-06-25 16:59:35 +0500

    This is the script file of this one-to-one chat application

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* =================================================================================== */
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyDiLhM-eNlinVjxUqZ5u_kiIowI105EcDQ",
@@ -8,6 +10,7 @@ var config = {
     messagingSenderId: "90533697446"
 };
 firebase.initializeApp(config);
+// Initialize Firebase Ends
 
 /* =================================================================================== */
 
@@ -23,7 +26,7 @@ function signUp() {
     var email = document.getElementById('email').value;
     var pass = document.getElementById('pwd').value;
 
-    console.log(email, name, pass)
+    // console.log(email, name, pass)
 
     if (!email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
         swal({
@@ -60,7 +63,7 @@ function signUp() {
                             text: "congratulations",
                             icon: "success",
                         });
-                        location = 'login.html'
+                        location = '../index.html'
                     })
             })
             .catch((error) => {
@@ -84,12 +87,12 @@ function logIn() {
     var email = document.getElementById('userEmail').value;
     var password = document.getElementById('userPwd').value;
 
-    console.log(email)
+    // console.log(email)
     auth.signInWithEmailAndPassword(email, password)
         .then(() => {
             document.getElementById('error').style.display = 'none'
-            console.log("success")
-            location = 'users.html'
+            // console.log("success")
+            location = '../index.html'
         })
         .catch((error) => {
             // Handle Errors here.
@@ -98,6 +101,57 @@ function logIn() {
         });
 }
 // login function
+
+/* =================================================================================== */
+
+// checking user
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log(user.email, '[current user]');
+        if (user.emailVerified === false) {
+            $("#allUsers").hide();
+            $("#friends").hide();
+            $("#room").show();
+            $("#verify").show();
+        }
+        $("#signUp").hide();
+        $("#login").hide();
+        $("#logout").show();
+    } else {
+        $("#allUsers").hide();
+        $("#friends").hide();
+        $("#room").hide();  
+        $("#signUp").show();
+        $("#login").show();
+        $("#logout").hide();
+        console.log("Please login!");
+    }
+});
+
+
+function sendVerificationLink() {
+    var user = firebase.auth().currentUser;
+    user.sendEmailVerification().then(function () {
+        swal('Done', `Email Verifcation Link has been sent to ${user.email}`, 'success');
+    }).catch(function (error) {
+        swal('Error!', `${error.message}`, "error");
+    })
+}
+
+function logOut() {
+    firebase.auth().signOut()
+        .then(function (resolve) {
+            window.location.replace("index.html");
+            console.log("Succesfully Signed-Out", resolve);
+        })
+        .catch(function (err) {
+            console.log("Error", err);
+        })
+}
+
+
+// checking user
 
 /* =================================================================================== */
 
@@ -111,7 +165,7 @@ function getAllUsers() {
                 let usersList = users.val()
                 // console.log(usersList, "usersList")
                 var currentuser = auth.currentUser.uid;
-                console.log(currentuser)
+                // console.log(currentuser)
                 user = currentuser
                 for (var key in usersList) {
                     // console.log(usersList[key])
@@ -121,7 +175,7 @@ function getAllUsers() {
                     }
 
                 }
-                console.log(usersArray, 'usersArray')
+                // console.log(usersArray, 'usersArray')
                 var ul = document.getElementById("usersList");
                 usersArray.map((v, i) => {
 
@@ -132,7 +186,7 @@ function getAllUsers() {
                     btn.appendChild(btnText)
                     btn.setAttribute("class", 'btn btn-info text-right')
                     btn.addEventListener("click", (e) => {
-                        console.log(v.uid)
+                        // console.log(v.uid)
 
                         firebaseDb.ref("users/" + currentuser + "/" + "chatRoom" + '/' + v.uid + '/').push('Wa-Alaikum-Assalam')
                             .then(() => {
@@ -163,32 +217,33 @@ function getAllUsers() {
 // get all users
 
 /* =================================================================================== */
-var frndUID;
+
 // get friends list
+var frndUID;
 function getFriendsList() {
 
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            console.log(user.uid, '[current user]')
+            // console.log(user.uid, '[current user]')
             let usersArray = []
             let currentUser = user.uid
             firebaseDb.ref("users/" + currentUser + '/' + 'chatRoom' + '/')
                 .once("value", (chat) => {
                     let roomData = chat.val();
-                    console.log(roomData, 'friendlist')
+                    // console.log(roomData, 'friendlist')
 
                     firebaseDb.ref("users/").once("value", (friends) => {
                         friendList = friends.val();
                         for (var key in roomData) {
-                            console.log(key, 'friend\'s uid');
-                            console.log(roomData[key], 'message');
+                            // console.log(key, 'friend\'s uid');
+                            // console.log(roomData[key], 'message');
                             if (currentUser !== key) {
                                 friendList[key].uid = key
-                                console.log(friendList[key].uid, "test")
+                                // console.log(friendList[key].uid, "test")
                                 usersArray.push(friendList[key])
                             }
                         }
-                        console.log(usersArray, 'db');
+                        // console.log(usersArray, 'db');
 
                         var ul = document.getElementById("friendslist");
                         usersArray.map((v, i) => {
@@ -201,7 +256,7 @@ function getFriendsList() {
                             btn.setAttribute("class", 'btn btn-success text-right')
                             btn.addEventListener("click", (uid) => {
                                 frndUID = v.uid;
-                                console.log(frndUID, 'selected friend\'s uid')
+                                // console.log(frndUID, 'selected friend\'s uid')
                                 localStorage.setItem('friendUID', frndUID);
                                 window.location = 'room.html'
                             })
@@ -230,13 +285,13 @@ function getConversation() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             var firendsUID = localStorage.getItem("friendUID");
-            console.log(firendsUID, "[friends uid]")
+            // console.log(firendsUID, "[friends uid]")
             let usersArray = []
             firebaseDb.ref("users/").once("value", (users) => {
                 let usersList = users.val()
                 // console.log(usersList, "usersList")
                 var currentuser = auth.currentUser.uid;
-                console.log(currentuser)
+                // console.log(currentuser)
                 user = currentuser
                 for (var key in usersList) {
                     // console.log(usersList[key])
@@ -244,7 +299,7 @@ function getConversation() {
                     usersArray.push(usersList[key])
 
                 }
-                console.log(usersArray, 'usersArray')
+                // console.log(usersArray, 'usersArray')
                 var ul = document.getElementById("chatRoom");
                 usersArray.map((v, i) => {
                     if (currentuser === v.uid) {
@@ -255,24 +310,24 @@ function getConversation() {
 
                         // console.log(v.uid)
                         firebaseDb.ref("users/" + v.uid + "/" + "chatRoom" + '/' + currentuser + '/').on('child_added', (CUmessages) => { // current user msgs
-                            console.log(CUmessages, 'cu msgs')
+                            // console.log(CUmessages, 'cu msgs')
                             localStorage.setItem('CUmsg', JSON.stringify(CUmessages.val()));
                             var userdata = localStorage.getItem('CUmsg');
                             userdata = JSON.parse(userdata);
                             var createdLi = crateElement(`${userdata}   (${currentName})`, 'LI', 'list-group-item')
-                            console.log('abcd', createdLi);
+                            // console.log('abcd', createdLi);
                             ul.appendChild(createdLi);
-                            console.log(userdata, 'cu data')
+                            // console.log(userdata, 'cu data')
                         })
                         firebaseDb.ref("users/" + currentuser + "/" + "chatRoom" + '/' + v.uid + '/').on('child_added', (FUmessages) => {  // friend (user) msgs
-                            console.log(FUmessages, 'fu msgs')
+                            // console.log(FUmessages, 'fu msgs')
                             localStorage.setItem('FUmsg', JSON.stringify(FUmessages.val()));
                             var userdata = localStorage.getItem('FUmsg');
                             userdata = JSON.parse(userdata);
                             var createdLi = crateElement(`${userdata}   (${v.name})`, 'LI', 'list-group-item')
-                            console.log('abcd', createdLi);
+                            // console.log('li', createdLi);
                             ul.appendChild(createdLi);
-                            console.log(userdata, 'fu data')
+                            // console.log(userdata, 'fu data')
 
                         })
                         function crateElement(text, element, className) {
@@ -291,31 +346,17 @@ function getConversation() {
         }
     });
 }
+// lets chat section end
 
+/* =================================================================================== */
 
+// Send Message
 var val = document.getElementById("messageBox");
-var btn = document.getElementById("sendMsgBtn");
-btn.addEventListener('click', (event) => {
-    // add(val.value);
-    sendMsg()
-    // val.value = "";
-});
 val.addEventListener('keydown', (event) => {
     if (event.keyCode === 13) {
-        // add(val.value);
         sendMsg();
-        // val.value = "";
     }
 });
-
-// var msgVal = $("#messageBox");
-// msgVal.addEventListener('keydown', (e) => {
-//     if (event.keyCode === 13) {
-//         $('#sendMsgBtn').addEventListener('click', e =>{
-//             sendMsg();
-//         })
-//     }
-// });
 
 function sendMsg() {
     var msg = $("#messageBox").val();
@@ -343,3 +384,6 @@ function sendMsg() {
         })
     })
 }
+// Send Message Section Ends
+
+/* =================================================================================== */
